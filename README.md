c7e52652330e8634af0e51216e90253e403c3f20
Jacky Kung <40329372+jackykwe@users.noreply.github.com>
2024-10-05 00:53:31 +0800

    Pretty table and sealed list done; first release

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Poweramp Helper
+## Installation
+1. Install Rust on your computer (`rustup` and `cargo`).
+2. Clone this repository.
+3. Run `cargo build --release`
+4. Use the compiled binary as follows (via `cargo run --release -- <args>`).
+
+## Workflow
+To generate auto-generated playlists:
+1. Clear `Poweramp Playlists Export` folder on device.
+2. Go into Poweramp > Settings > Library > Playlists > Export Poweramp Playlists > choose Target Folder as `Poweramp Playlists Export` > check "Also export file based playlists (.m3u8, .pls, etc.)" > Export.
+3. Zip and transfer the `Poweramp Playlists Export` to the computer (at some local location), then extract.
+4. Run poweramp-helper, with `-d` pointing at the local location. This will generate `[Auto] <...>.m3u8` files into the local location. You will be prompted before any overwrites happen.
+   ```
+   cargo run --release -- -d <local_location>
+   ```
+5. Zip and transfer the local location back to the device.
+6. Clear `Poweramp Playlists Import` folder on device.
+7. Extract the transferred zip on the device into `Poweramp Playlists Import`.
+8. Go into Poweramp > Settings > Library > Rescan.
+
+The auto-generated playlists should be updated in the application.
+
+More usage instructions can be viewed by running the program with the `-h/--help` flag.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
+    collections::VecDeque,
     fs::File,
-    io::{BufRead, BufReader, BufWriter, Write},
+    io::{BufRead, BufReader, BufWriter, Read, Write},
     iter::Peekable,
     path::{Path, PathBuf},
 };
@@ -9,7 +10,7 @@ use clap::Parser;
 use polars::{
     df,
     io::SerWriter,
-    prelude::{all, col, concat_str, lit, DataType, Expr, IntoLazy, LazyFrame},
+    prelude::{all, col, concat_str, lit, DataType, Expr, IntoLazy, IntoVec, LazyFrame},
     series::Series,
 };
 
@@ -120,7 +121,7 @@ fn read_and_parse_m3u8_all(m3u8_dir: &Path) -> anyhow::Result<LazyFrame> {
         col("line_rating")
             .str()
             .tail(lit(1))
-            .strict_cast(DataType::UInt32) // see https://docs.pola.rs/user-guide/expressions/casting/#strings
+            .strict_cast(DataType::UInt8) // see https://docs.pola.rs/user-guide/expressions/casting/#strings
             .alias("rating"),
         col("line_file_name")
             .str()
@@ -210,6 +211,168 @@ fn generate_and_write_auto_m3u8_playlist(
     Ok(())
 }
 
+fn materialise_lazyframe_into_csv_lines(df: LazyFrame) -> anyhow::Result<VecDeque<u8>> {
+    let mut statistics_table = df
+        .clone()
+        .group_by(["parent_dir"])
+        .agg([
+            col("rating").eq(lit(0)).sum().alias("0S"),
+            col("rating").eq(lit(1)).sum().alias("1S"),
+            col("rating").eq(lit(2)).sum().alias("2S"),
+            col("rating").eq(lit(3)).sum().alias("3S"),
+            col("rating").eq(lit(4)).sum().alias("4S"),
+            col("rating").eq(lit(5)).sum().alias("5S"),
+            col("EN").sum(),
+            col("CN").sum(),
+            col("JP").sum(),
+            col("KR").sum(),
+            col("O").sum(),
+            col("file_name").count().alias("Total"),
+        ])
+        // no need to .select([all().exclude([...])]) because agg() implicitly only
+        // selects the provided columns above
+        .sort(["parent_dir"], Default::default())
+        .rename(["parent_dir"], ["Directory"])
+        .collect()?;
+
+    // Export DataFrame to csv format for quicker row-wise iteration (polars supports
+    // only fast column-wise iteration since data is stored column-wise), as recommended
+    // by polars's documentation.
+    let mut in_memory = VecDeque::with_capacity(statistics_table.estimated_size());
+    polars::prelude::CsvWriter::new(BufWriter::new(&mut in_memory))
+        .include_header(true)
+        .with_separator(b',')
+        .with_quote_char(b'"')
+        .with_quote_style(polars::prelude::QuoteStyle::Always)
+        .finish(&mut statistics_table)?;
+    Ok(in_memory)
+}
+
+fn get_sealed_parent_dirs() -> anyhow::Result<Option<Vec<String>>> {
+    // Read sealed.txt if it exists
+    let sealed_txt_path = PathBuf::from("sealed.txt");
+    Ok(if sealed_txt_path.exists() {
+        Some(
+            #[allow(clippy::result_filter_map)]
+            BufReader::new(File::open("sealed.txt")?)
+                .lines()
+                .filter(Result::is_ok)
+                .map(Result::unwrap) // would have used .flatten() but that has the danger of `flatten()` will run forever if the iterator repeatedly produces an `Err`
+                .collect(),
+        )
+    } else {
+        None
+    })
+}
+
+fn construct_pretty_table(
+    csv_lines: impl Read,
+    sealed_parent_dirs: &Option<Vec<String>>,
+) -> anyhow::Result<comfy_table::Table> {
+    fn red_cell_if_non_zero(value: usize, cell_suffix: Option<&str>) -> comfy_table::Cell {
+        let displayed = match cell_suffix {
+            None => format!("{}", value),
+            Some(cell_suffix) => format!("{}{}", value, cell_suffix),
+        };
+        if value == 0 {
+            comfy_table::Cell::new(displayed)
+        } else {
+            comfy_table::Cell::new(displayed)
+                .fg(comfy_table::Color::Red)
+                .add_attributes(vec![
+                    comfy_table::Attribute::Bold,
+                    comfy_table::Attribute::SlowBlink,
+                ])
+        }
+    }
+
+    /// Would have used `&str` in argument types instead of String but that makes things
+    /// harder for calling code. Perhaps something to fix in the future (not necessary now
+    /// since the calling flexibility gained from using `&str` isn't required at the moment;
+    /// cf. deref coercion). The problem is that `Vec<&str>` is hard to obtain from a
+    /// `Vec<String>`.
+    fn red_cell_if_not_in(
+        display_value: String,
+        parent_dir: &String,
+        sealed_parent_dirs: &Option<Vec<String>>,
+    ) -> comfy_table::Cell {
+        match sealed_parent_dirs {
+            Some(sealed_parent_dirs) if !sealed_parent_dirs.contains(parent_dir) => {
+                comfy_table::Cell::new(display_value)
+                    .fg(comfy_table::Color::Red)
+                    .add_attributes(vec![
+                        comfy_table::Attribute::Bold,
+                        comfy_table::Attribute::RapidBlink,
+                    ])
+            }
+            Some(_) | None => comfy_table::Cell::new(display_value),
+        }
+    }
+
+    let mut pretty_table = comfy_table::Table::new();
+    pretty_table
+        .load_preset(comfy_table::presets::UTF8_FULL)
+        .apply_modifier(comfy_table::modifiers::UTF8_ROUND_CORNERS)
+        .set_content_arrangement(comfy_table::ContentArrangement::DynamicFullWidth)
+        .set_header([
+            comfy_table::Cell::new("Directory").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("0S").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("1S").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("2S").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("3S").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("4S").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("5S").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("EN").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("CN").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("JP").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("KR").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("O").add_attribute(comfy_table::Attribute::Bold),
+            comfy_table::Cell::new("Total").add_attribute(comfy_table::Attribute::Bold),
+        ]);
+    for row in csv::Reader::from_reader(BufReader::new(csv_lines))
+        .records()
+        .flatten()
+    {
+        // "Directory","0S","1S","2S","3S","4S","5S","EN","CN","JP","KR","O","Total"
+        let row = row.into_vec();
+        let parent_dir = &row[0].to_string();
+        let zero_stars_count: usize = row[1].parse()?;
+        let one_star_count: usize = row[2].parse()?;
+        let two_stars_count: usize = row[3].parse()?;
+        let three_stars_count: usize = row[4].parse()?;
+        let four_stars_count: usize = row[5].parse()?;
+        let five_stars_count: usize = row[6].parse()?;
+        let en_count: usize = row[7].parse()?;
+        let cn_count: usize = row[8].parse()?;
+        let jp_count: usize = row[9].parse()?;
+        let kr_count: usize = row[10].parse()?;
+        let o_count: usize = row[11].parse()?;
+        let total_count: usize = row[12].parse()?;
+        pretty_table.add_row([
+            comfy_table::Cell::new(parent_dir),
+            red_cell_if_non_zero(zero_stars_count, Some(" 零")),
+            comfy_table::Cell::new(format!("{} 壱", one_star_count)),
+            comfy_table::Cell::new(format!("{} 弐", two_stars_count)),
+            comfy_table::Cell::new(format!("{} 参", three_stars_count)),
+            comfy_table::Cell::new(format!("{} 肆", four_stars_count)),
+            comfy_table::Cell::new(format!("{} 伍", five_stars_count)),
+            red_cell_if_not_in(format!("{} E", en_count), parent_dir, sealed_parent_dirs),
+            red_cell_if_not_in(format!("{} 中", cn_count), parent_dir, sealed_parent_dirs),
+            red_cell_if_not_in(format!("{} 日", jp_count), parent_dir, sealed_parent_dirs),
+            red_cell_if_not_in(format!("{} 한", kr_count), parent_dir, sealed_parent_dirs),
+            red_cell_if_not_in(format!("{} O", o_count), parent_dir, sealed_parent_dirs),
+            comfy_table::Cell::new(format!("{} Σ", total_count)),
+        ]);
+    }
+    for column_index in 1..=12 {
+        pretty_table
+            .column_mut(column_index)
+            .unwrap()
+            .set_cell_alignment(comfy_table::CellAlignment::Right);
+    }
+    Ok(pretty_table)
+}
+
 fn main() -> anyhow::Result<()> {
     let args: Args = Args::parse();
     check_required_m3u8s_exist(&args.m3u8_dir)?;
@@ -240,76 +403,10 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    let mut statistics_table = df
-        .clone()
-        .group_by(["parent_dir"])
-        .agg([
-            col("rating").eq(lit(0)).sum().alias("0S"),
-            col("rating").eq(lit(1)).sum().alias("1S"),
-            col("rating").eq(lit(2)).sum().alias("2S"),
-            col("rating").eq(lit(3)).sum().alias("3S"),
-            col("rating").eq(lit(4)).sum().alias("4S"),
-            col("rating").eq(lit(5)).sum().alias("5S"),
-            col("EN").sum(),
-            col("CN").sum(),
-            col("JP").sum(),
-            col("KR").sum(),
-            col("O").sum(),
-            col("file_name").count().alias("Total"),
-        ])
-        // no need to .select([all().exclude([...])]) because agg() implicitly only
-        // selects the provided columns above
-        .sort(["parent_dir"], Default::default())
-        .rename(["parent_dir"], ["Directory"])
-        .collect()?;
-    polars::prelude::CsvWriter::new(File::create("statistics_table.csv")?)
-        .include_header(true)
-        .with_separator(b',')
-        .with_quote_char(b'"')
-        .with_quote_style(polars::prelude::QuoteStyle::Always)
-        .finish(&mut statistics_table)?;
+    let df_csv = materialise_lazyframe_into_csv_lines(df)?;
+    let sealed_parent_dirs = get_sealed_parent_dirs()?;
+    let pretty_table = construct_pretty_table(df_csv, &sealed_parent_dirs)?;
 
-    let mut pretty_table = comfy_table::Table::new();
-    pretty_table
-        .load_preset(comfy_table::presets::UTF8_FULL)
-        .apply_modifier(comfy_table::modifiers::UTF8_ROUND_CORNERS)
-        .set_content_arrangement(comfy_table::ContentArrangement::DynamicFullWidth)
-        .set_header([
-            comfy_table::Cell::new("Directory").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("0S").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("1S").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("2S").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("3S").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("4S").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("5S").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("EN").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("CN").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("JP").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("KR").add_attribute(comfy_table::Attribute::Bold),
-            comfy_table::Cell::new("O").add_attribute(comfy_table::Attribute::Bold),
-        ]);
-    for row in csv::Reader::from_reader(BufReader::new(File::open("statistics_table.csv")?))
-        .records()
-        .flatten()
-    {
-        // "Directory","0S","1S","2S","3S","4S","5S","EN","CN","JP","KR","O","Total"
-        pretty_table.add_row([
-            row.get(0).unwrap(),
-            row.get(1).unwrap(),
-            row.get(2).unwrap(),
-            row.get(3).unwrap(),
-            row.get(4).unwrap(),
-            row.get(5).unwrap(),
-            row.get(6).unwrap(),
-            row.get(7).unwrap(),
-            row.get(8).unwrap(),
-            row.get(9).unwrap(),
-            row.get(10).unwrap(),
-            row.get(11).unwrap(),
-            row.get(12).unwrap(),
-        ]);
-    }
     println!("{}", pretty_table);
-
     Ok(())
 }
